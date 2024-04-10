63b9d05ca80a2ab78325da2db71ccf9a8bc6666c
Hannah <120557473+ccbyjin@users.noreply.github.com>
2024-04-10 14:48:46 +0800

    new file with bug

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,28 @@ const changeInput = (val, id) => {
   labelForm[id] = val;
 };
 
+
+function cleanHandle(){
+  
+  // 清空 input 值
+  labelForm.startingAccount = "";
+  labelForm.accountNumber = "";
+  resultArr.value = [];
+  
+// 處理錯誤訊息
+  if (errorMessage.value.length !== 0) {
+    // 設置定時器，在三秒後清空 errorMessage
+    setTimeout(() => {
+      errorMessage.value = "";
+    }, 3000);
+  }
+}
+
 function clearInput() {
   // 清空 input 值
-    labelForm.startingAccount = "";
-    labelForm.accountNumber = "";
-    resultArr.value = [];
+  labelForm.startingAccount = "";
+  labelForm.accountNumber = "";
+  resultArr.value = [];
 }
 
 // 處理錯誤訊息
@@ -32,40 +49,48 @@ function errorHandle() {
     }, 3000);
   }
 }
+
+// 起始帳號規則
+const startingAccountLengthRegex = /^\d{11}$/;
+const startingAccountNumberRegex = /^\d+$/;
+// 帳號數量規則
+const accountNumberRegex = /^[1-9]\d{0, 3}$/;
+// 分行別規則
+const branchRegex = /^(0[1-9]|[1-8][0-9]|9[0-8])$/;
+// 科目別規則
+const subjectRegex = /^(10|20|30|40|50|51|66|77|88)/;
+
+
 // 驗證表單
 const validateForm = () => {
   // 驗證表單，全部通過返回 true
   errorMessage.value = ""; // 用於重置錯誤信息，否則會bug
 
-  // 驗證帳號輸入數量
-  if (labelForm.accountNumber < 1 || labelForm.accountNumber > 9999) {
-    errorMessage.value = "帳號數量錯誤";
-    clearInput();
-    errorHandle();
+  labelForm.startingAccount = labelForm.startingAccount.trim();
+  // 驗證起始帳號長度 + 輸入
+  if (!startingAccountLengthRegex.test(labelForm.startingAccount)) {
+    errorMessage.value = "起始帳號長度錯誤";
+    cleanHandle();
+    return;
+  }
+  if (!startingAccountNumberRegex.test(labelForm.startingAccount)) {
+    errorMessage.value = "起始帳號需為數字";
+    cleanHandle();
     return;
   }
 
-  labelForm.startingAccount = labelForm.startingAccount.trim();
-  // 驗證起始帳號長度 + 輸入
-  if (labelForm.startingAccount.length !== 11) {
-    errorMessage.value = "起始帳號長度錯誤";
-    clearInput();
-    errorHandle();
-    return;
-  }
-  if (!/^\d+$/.test(labelForm.startingAccount)) {
-    errorMessage.value = "起始帳號需為數字";
-    clearInput();
-    errorHandle();
+  // 驗證帳號輸入數量
+  if (accountNumberRegex.test(labelForm.accountNumber)) {
+    errorMessage.value = "帳號數量錯誤";
+    cleanHandle();
     return;
   }
 
   // 分行別驗證
   const branch = parseInt(labelForm.startingAccount.slice(0, 3));
-  if (branch < 1 || branch > 99 || branch === 19) {
+  if (branchRegex.test(branch) || branch === 19) {
     errorMessage.value = "分行別錯誤";
-    clearInput();
-    errorHandle();
+    cleanHandle();
     return;
   }
 
@@ -74,10 +99,9 @@ const validateForm = () => {
   for (let i = 0; i < labelForm.accountNumber; i++) {
     // 驗證科目別
     const subject = parseInt(labelForm.startingAccount.slice(3, 5));
-    if (![10, 20, 30, 40, 50, 51, 66, 77, 88].includes(subject)) {
+    if (!subjectRegex.test(subject)) {
       errorMessage.value = "生成帳號將造成科目別錯誤";
-      clearInput(); 
-      errorHandle();
+      cleanHandle();
       return;
     }
     // 分別丟入函數運算得出檢核碼
@@ -153,7 +177,7 @@ const validateForm = () => {
         </li>
       </ul>
       <!-- 點擊按鈕時觸發 clearInput 方法 -->
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-10 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-200 to-lime-200 group-hover:from-teal-200 group-hover:to-lime-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" @click="clearInput">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-10 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-200 to-lime-200 group-hover:from-teal-200 group-hover:to-lime-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" @click="cleanHandle">
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
           Clear
         </span>
